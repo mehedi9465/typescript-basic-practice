9fddae7bc7679536439293d78cf73590379d6816
Mehedi-Hasan-Choyon <34033574+Mehedi-Hasan-Choyon@users.noreply.github.com>
2021-12-29 07:24:55 +0600

    first push

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+let id = 5;
+let company = "Programming Hero";
+let isGood = true;
+let x = 'Hello';
+let arr = [1, 2, 3, 4]; // array of any types
+let arr1 = [1, 2, 3, 4]; // array of number type
+let arr2 = [1, "2", true]; // tuple
+let arr3; // two dimensional array
+let obj;
+obj = [
+    { "name": "Mehedi" },
+    { "name": "Hasan" },
+    { "name": "Choyon" }
+];
+console.log(obj);
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+let id: number = 5
+let company: string = "Programming Hero"
+let isGood: boolean = true
+let x: any = 'Hello';
+
+let arr: any[] = [1, 2, 3, 4] // array of any types
+let arr1: number[] = [1, 2, 3, 4] // array of number type
+let arr2: [number, string, boolean ] = [1, "2", true]  // tuple
+let arr3: [number, string][] // two dimensional array
+let obj: {}[];
+obj = [
+    {"name": "Mehedi"},
+    {"name": "Hasan"},
+    {"name": "Choyon"}
+]
+console.log(obj);
+
+let studID: string | number // Union
+studID = "true";
+
+
